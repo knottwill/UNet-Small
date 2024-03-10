3371176942db8fff0e521c2c0bcf4408ccdcd190
knottwill <81773569+knottwill@users.noreply.github.com>
2024-03-10 13:01:07 +0000

    implemented Dataset

---
+++ src/data_loading.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset
+
+
+class LCTDataset(Dataset):
+    """!
+    @brief LCTDataset class for loading 2D slices from the dataset.
+    """
+    def __init__(self, dataroot, cases, transform=None):
+        self.dataroot = dataroot
+        self.transform = transform
+
+        images, masks = torch.tensor([]), torch.tensor([])
+        for case in cases:
+            # load images and masks
+            case_images = np.load(os.path.join(dataroot, "Images", f'{case}.npz'))['images']
+            case_masks = np.load(os.path.join(dataroot, "Segmentations", f'{case}_seg.npz'))['masks']
+
+            # convert to tensors
+            case_images = torch.tensor(case_images, dtype=torch.float32).unsqueeze(1)
+            case_masks = torch.tensor(case_masks, dtype=torch.float32).unsqueeze(1)
+
+            # concatenate
+            images = torch.cat((images, case_images))
+            masks = torch.cat((masks, case_masks))
+
+        assert images.shape == masks.shape, "Images and masks should have the same shape"
+
+        self.images = images
+        self.masks = masks
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+
+        image, mask = self.images[idx], self.masks[idx]
+
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        return image, mask
