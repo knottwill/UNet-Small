8a647d43b8307a6f2d06a8e03c7ef113bc2d114d
knottwill <81773569+knottwill@users.noreply.github.com>
2024-03-15 10:54:17 +0000

    Implemented prediction script

---
+++ scripts/predict.py
@@ -0,0 +1,52 @@
+"""
+
+python scripts/predict.py --dataroot ./Dataset --model_state_dict ./Models/UNet.pt --output_dir ./Predictions \
+--cases all --prediction_type prob --device cpu
+"""
+
+import os
+import sys
+
+# add project root to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_root)
+
+import numpy as np
+from tqdm import tqdm
+import torch
+from torch.utils.data import DataLoader
+
+from src.model import UNet
+from src.data_loading import LCTDataset
+from src.arguments import parse_args
+
+args = parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+# load pre-trained model
+model = UNet(in_channels=1, out_channels=1).to(device)
+model.load_state_dict(torch.load(args.model_state_dict))
+
+model.eval()
+for case in args.cases:
+    case_dataset = LCTDataset(args.dataroot, [case])
+    dataloader = DataLoader(case_dataset, batch_size=1, shuffle=False)
+
+    # get predictions for all images in the case
+    probs = torch.tensor([])
+    for x, masks in tqdm(dataloader):
+        x, masks = x.to(device), masks.to(device)
+        pred = model(x).detach().cpu()
+        probs = torch.cat((probs, pred), 0)
+
+    save_path = os.path.join(args.output_dir, f"{case}_pred.npz")
+
+    if args.prediction_type == "prob":
+        probs = probs.squeeze().detach().cpu().numpy()
+        np.savez(save_path, probs=probs)
+
+    elif args.prediction_type == "mask":
+        masks_pred = (probs > 0.5).float()
+        masks_pred = masks_pred.squeeze().detach().cpu().numpy()
+        np.savez(save_path, masks=masks_pred)
--- src/arguments.py
+++ src/arguments.py
@@ -1,6 +1,26 @@
 import argparse
 
 
+def parse_cases(args):
+    """Parse the 'cases' argument which specify the cases to predict on"""
+
+    if args.cases == "train":
+        args.cases = [f"Case_{i:03}" for i in range(0, 8)]
+    elif args.cases == "test":
+        args.cases = [f"Case_{i:03}" for i in range(8, 12)]
+    elif args.cases == "all":
+        args.cases = [f"Case_{i:03}" for i in range(0, 12)]
+    else:
+        case_nums = args.cases.split(",")
+        args.cases = []
+        for num in case_nums:
+            num = int(num)
+            assert num >= 0 and num < 11, "Invalid case number"
+            args.cases.append(f"Case_{num:03}")
+
+    return args
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
 
@@ -16,5 +36,18 @@ def parse_args():
                         on the test set at each epoch of the training process",
     )
 
+    # specific to predict.py
+    parser.add_argument("--model_state_dict", type=str, help="path to the trained model state dict file")
+    parser.add_argument(
+        "--cases",
+        default="test",
+        type=str,
+        help="train, test, all, or specific case numbers \
+                        eg. 0,3,5 for Case_000, Case_003, Case_005",
+    )
+    parser.add_argument("--prediction_type", default="prob", type=str, help="'prob' for probabilities or 'mask' for binary masks")
+
     args = parser.parse_known_args()[0]
+    args = parse_cases(args)
+
     return args
