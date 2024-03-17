ced79973341dd80905b981f8a44a275c309cd5f6
knottwill <81773569+knottwill@users.noreply.github.com>
2024-03-17 16:26:29 +0000

    Changed analysis.py to two separate scripts, one for statistics and one for plots

--- scripts/make_plots.py
+++ scripts/make_plots.py
@@ -1,6 +1,6 @@
-"""!@file analysis.py
+"""!@file make_plots.py
 
-python scripts/analysis.py --dataroot ./Dataset --predictions_dir ./Predictions --metric_logger ./Models/metric_logger.pkl --output_dir ./plots
+python scripts/make_plots.py --dataroot ./Dataset --predictions_dir ./Predictions --metric_logger ./Models/metric_logger.pkl --output_dir ./plots
 """
 
 import os
@@ -32,49 +32,16 @@ df = construct_results_dataframe(args.dataroot, args.predictions_dir)
 test_filter = df["Set"] == "test"
 train_filter = df["Set"] == "train"
 
-# First we split the dataframe into sets with and without ground-truth masks
-test_no_mask = df[test_filter][df[test_filter]["Mask Size"] == 0]
-test_with_mask = df[test_filter][df[test_filter]["Mask Size"] > 0]
-train_no_mask = df[train_filter][df[train_filter]["Mask Size"] == 0]
-train_with_mask = df[train_filter][df[train_filter]["Mask Size"] > 0]
-
-empty_proportion_test = len(test_no_mask) / len(df[test_filter])
-empty_proportion_train = len(train_no_mask) / len(df[train_filter])
-
-###################
-# Main Statistics
-###################
-
-print(f"Proportion of empty-mask images in test set: {empty_proportion_test:.5f}")
-print(f"Proportion of empty-mask images in train set: {empty_proportion_train:.5f}\n")
-print(f"Mean DSC for images with masks in test set: {test_with_mask['DSC'].mean():.5f}")
-print(f"Mean DSC for images with masks in train set: {train_with_mask['DSC'].mean():.5f}\n")
-print(f'Mean accuracy overall in test set: {df[test_filter]["Accuracy"].mean():.5f}')
-print(f'Mean accuracy overall in train set: {df[train_filter]["Accuracy"].mean():.5f}\n')
-
-print(f'Mean accuracy for empty masks in test set: {test_no_mask["Accuracy"].mean():.5f}')
-empty_correctly_predicted = len(test_no_mask[test_no_mask["Accuracy"] == 1]) / len(test_no_mask)
-print(f"Proportion of empty masks in test set with perfect accuracy: {empty_correctly_predicted:.5f}\n")
-
-# calculate recall and specificity in test set
-masks = np.stack(df[test_filter]["Mask"].to_numpy()).flatten()
-probs = np.stack(df[test_filter]["Prediction"].to_numpy()).flatten()
-pred = (probs > 0.5).astype(int)
-
-recall = (masks * pred).sum() / masks.sum()
-specificity = ((1 - masks) * (1 - pred)).sum() / (1 - masks).sum()
-
-print(f"Recall in test set: {recall:.5f}")
-print(f"Specificity in test set: {specificity:.5f}\n")
-
-print(f"Proportion of positive (mask) pixels in test set: {(masks == 1).sum()/len(masks):.5f}")
-
 ################
 # Plot loss, DSC and accuracy over training
 ################
 
 with open(args.metric_logger, "rb") as f:
     metric_logger = pickle.load(f)
+
+# Calculate the proportion of empty-mask images in the test and train sets
+empty_proportion_test = (df[test_filter]["Mask Size"] == 0).sum() / len(df[test_filter])
+empty_proportion_train = (df[train_filter]["Mask Size"] == 0).sum() / len(df[train_filter])
 
 fig = plot_metric_logger(metric_logger, empty_proportion_test, empty_proportion_train)
 
@@ -176,6 +143,13 @@ fig.savefig(os.path.join(args.output_dir, "heatmaps.png"))
 ###########
 
 print("Plotting precision-recall curve... (may take a few minutes)")
+
+# get masks and predictions for whole test set
+masks = np.stack(df[test_filter]["Mask"].to_numpy()).flatten()
+probs = np.stack(df[test_filter]["Prediction"].to_numpy()).flatten()
+pred = (probs > 0.5).astype(int)
+
+# precision-recall curve
 precision, recall, _ = precision_recall_curve(masks, probs)
 AUC = auc(recall, precision)
 
@@ -189,4 +163,4 @@ ax.set_xlim([0.0, 1.05])
 ax.set_title(f"AUC={AUC}")
 
 # save figure
-fig.savefig(os.path.join(args.output_dir, "precision_recall_curve.png"))
+fig.savefig(os.path.join(args.output_dir, "PR-curve.png"))
