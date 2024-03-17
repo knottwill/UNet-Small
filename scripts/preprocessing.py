791d978878a2f800561263181f7fc67d8b916363
knottwill <81773569+knottwill@users.noreply.github.com>
2024-03-17 18:32:33 +0000

    Added de-identification to preprocessing script

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,6 +1,6 @@
 """!@file preprocessing.py
 
-@brief Script converts the DICOM dataset into numpy arrays per case (patient),
+@brief Script de-identifies the dicom files and converts them into numpy arrays per case (patient),
 aligned with the segmentation arrays, and saves them in npz files.
 
 @details For all cases EXCEPT Case_011, the dicom files have the naming convention
@@ -13,12 +13,13 @@ python scripts/preprocessing.py --dataroot ./Dataset
 """
 
 import os
+from os.path import join
 import sys
 import numpy as np
 from pydicom import dcmread
 
 # add project root to sys.path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+project_root = os.path.abspath(join(os.path.dirname(__file__), ".."))
 sys.path.append(project_root)
 
 from src.utils import slice_num
@@ -26,12 +27,56 @@ from src.arguments import parse_args
 
 args = parse_args()
 
+##################
+# De-identification
+##################
+
+modified_values = []
+for dir, subdirs, filenames in os.walk(join(args.dataroot, "Images")):
+    for filename in filenames:
+        if filename.endswith(".dcm"):
+            dcm = dcmread(join(dir, filename))
+            case_id = os.path.basename(dir)
+
+            modified = False
+
+            if dcm["PatientID"].value != case_id:
+                dcm["PatientID"].value = case_id
+                modified_values.append(f"{case_id} - PatientID")
+                modified = True
+
+            if dcm["PatientName"].value != case_id:
+                dcm["PatientName"].value = case_id
+                modified_values.append(f"{case_id} - PatientName")
+                modified = True
+
+            if dcm["PatientBirthDate"].value != "":
+                dcm["PatientBirthDate"].value = ""
+                modified_values.append(f"{case_id} - PatientBirthDate")
+                modified = True
+
+            if "PatientBirthTime" in dcm:
+                del dcm["PatientBirthTime"]
+                modified_values.append(f"{case_id} - PatientBirthTime")
+                modified = True
+
+            if modified:
+                dcm.save_as(join(dir, filename))
+
+modified_values = set(modified_values)
+for value in modified_values:
+    print("De-identified: ", value)
+
+##################
+# Conversion to numpy arrays per case
+##################
+
 # get cases
-items = sorted(os.listdir(os.path.join(args.dataroot, "Images")))
-cases = [item for item in items if os.path.isdir(os.path.join(args.dataroot, "Images", item))]
+items = sorted(os.listdir(join(args.dataroot, "Images")))
+cases = [item for item in items if os.path.isdir(join(args.dataroot, "Images", item))]
 
 for case in cases:
-    case_dir = os.path.join(args.dataroot, "Images", case)
+    case_dir = join(args.dataroot, "Images", case)
 
     # get sorted list of dicom files in case directory, ordered in the
     # same order as the masks in the .npz files
@@ -43,10 +88,10 @@ for case in cases:
     # get array of images (CT scans)
     images = np.zeros((len(case_files), 512, 512))
     for i, file in enumerate(case_files):
-        dcm = dcmread(os.path.join(case_dir, file))
+        dcm = dcmread(join(case_dir, file))
         images[i] = dcm.pixel_array
 
     # save array in npz file
-    filepath = os.path.join(args.dataroot, "Images", f"{case}.npz")
+    filepath = join(args.dataroot, "Images", f"{case}.npz")
     np.savez(filepath, images=images)
     print("Saved: ", filepath)
