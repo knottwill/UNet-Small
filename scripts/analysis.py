44253484c2901836ca7b56e24c891a18b07d1399
knottwill <81773569+knottwill@users.noreply.github.com>
2024-03-18 12:00:05 +0000

    Moved get_statistics and make_plots into the same file (analysis.py)

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,6 +1,6 @@
-"""!@file make_plots.py
+"""!@file analysis.py
 
-python scripts/make_plots.py --dataroot ./Dataset --predictions_dir ./Predictions --metric_logger ./Models/metric_logger.pkl --output_dir ./plots
+python scripts/analysis.py --dataroot ./Dataset --predictions_dir ./Predictions --metric_logger ./Models/metric_logger.pkl --output_dir ./plots
 """
 
 import os
@@ -37,6 +37,40 @@ with open("train_test_split.json", "r") as f:
 df = construct_results_dataframe(args.dataroot, args.predictions_dir, train_cases, test_cases)
 test_filter = df["Set"] == "test"
 train_filter = df["Set"] == "train"
+
+###################
+# Main Statistics
+###################
+
+# First we split the dataframe into sets with and without ground-truth masks
+test_no_mask = df[test_filter][df[test_filter]["Mask Size"] == 0]
+test_with_mask = df[test_filter][df[test_filter]["Mask Size"] > 0]
+train_no_mask = df[train_filter][df[train_filter]["Mask Size"] == 0]
+train_with_mask = df[train_filter][df[train_filter]["Mask Size"] > 0]
+
+print("\n###########################################################")
+print("##################### Main Statistics #####################\n")
+
+print(f"Mean DSC for images with masks in test set: {test_with_mask['DSC'].mean():.5f}")
+print(f"Mean DSC for images with masks in train set: {train_with_mask['DSC'].mean():.5f}\n")
+print(f'Mean accuracy overall in test set: {df[test_filter]["Accuracy"].mean():.5f}')
+print(f'Mean accuracy overall in train set: {df[train_filter]["Accuracy"].mean():.5f}\n')
+
+print(f'Mean accuracy for empty masks in test set: {test_no_mask["Accuracy"].mean():.5f}')
+empty_correctly_predicted = len(test_no_mask[test_no_mask["Accuracy"] == 1]) / len(test_no_mask)
+print(f"Proportion of empty masks in test set with perfect accuracy: {empty_correctly_predicted:.5f}\n")
+
+# calculate recall and specificity in test set
+masks = np.stack(df[test_filter]["Mask"].to_numpy()).flatten()
+probs = np.stack(df[test_filter]["Prediction"].to_numpy()).flatten()
+pred = (probs > 0.5).astype(int)
+
+recall = (masks * pred).sum() / masks.sum()
+specificity = ((1 - masks) * (1 - pred)).sum() / (1 - masks).sum()
+
+print(f"Recall in test set: {recall:.5f}")
+print(f"Specificity in test set: {specificity:.5f}\n")
+print("###########################################################\n")
 
 ################
 # Plot loss, DSC and accuracy over training
@@ -149,11 +183,6 @@ fig.savefig(os.path.join(args.output_dir, "heatmaps.png"))
 ###########
 
 print("Plotting precision-recall curve... (may take a few minutes)")
-
-# get masks and predictions for whole test set
-masks = np.stack(df[test_filter]["Mask"].to_numpy()).flatten()
-probs = np.stack(df[test_filter]["Prediction"].to_numpy()).flatten()
-pred = (probs > 0.5).astype(int)
 
 # precision-recall curve
 precision, recall, _ = precision_recall_curve(masks, probs)
